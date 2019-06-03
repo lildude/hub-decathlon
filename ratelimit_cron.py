051af3495e0a91fcced7e19c5b476aec86c8f2a1
Charles <canssens@gmail.com>
2019-06-03 23:31:23 +0200

    show services in rate limit cron

--- ratelimit_cron.py
+++ ratelimit_cron.py
@@ -2,4 +2,5 @@ from tapiriik.services import Service
 from tapiriik.services.ratelimiting import RateLimit
 
 for svc in Service.List():
+	print("service : " + svc.DisplayName)
 	RateLimit.Refresh(svc.ID, svc.GlobalRateLimits)
