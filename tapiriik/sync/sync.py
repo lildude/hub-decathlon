cba8cdaa1017826074b58328f02a563c321cb6a6
Charles <canssens@gmail.com>
2020-12-03 16:03:42 +0100

    Rise up the delay to sync and add random effect

--- tapiriik/sync/sync.py
+++ tapiriik/sync/sync.py
@@ -93,7 +93,8 @@ def _unpackUserException(raw):
 class Sync:
 
     _sqs_manager = None
-    SyncInterval = timedelta(hours=24) #auto sync each interval
+    SyncIntervalHours = 48 + random.randint(1,48)
+    SyncInterval = timedelta(hours=SyncIntervalHours) #auto sync each interval
     SyncIntervalJitter = timedelta(minutes=5)
     MinimumSyncInterval = timedelta(seconds=30)
     MaximumIntervalBeforeExhaustiveSync = timedelta(days=14)  # Based on the general page size of 50 activites, this would be >3/day...
