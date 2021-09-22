4d672df1a280a75fd6235887ff47fa5b84d0bdfe
Charles <canssens@gmail.com>
2021-09-22 10:11:33 +0200

    Change to sync with small delay 1s

--- tapiriik/sync/sync.py
+++ tapiriik/sync/sync.py
@@ -96,7 +96,7 @@ class Sync:
     SyncIntervalHours = 96 + random.randint(1,48)
     SyncInterval = timedelta(hours=SyncIntervalHours) #auto sync each interval
     SyncIntervalJitter = timedelta(minutes=5)
-    MinimumSyncInterval = timedelta(seconds=60)
+    MinimumSyncInterval = timedelta(seconds=1)
     MaximumIntervalBeforeExhaustiveSync = timedelta(days=30)  # Based on the general page size of 50 activites, this would be >3/day...
 
     # TODO : change sync to real class
