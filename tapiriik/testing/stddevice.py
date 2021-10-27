11498749035e085bcfc65bdfc30ec3e904039458
kevforget <kevin.forget.partner@decathlon.com>
2021-10-27 17:22:33 +0200

    test: wip unknown manufacturer test

--- tapiriik/testing/stddevice.py
+++ tapiriik/testing/stddevice.py
@@ -79,3 +79,28 @@ class StdDeviceTest(TestCase):
         self.assertEqual(hubDevice.Manufacturer, "decathlon")
         self.assertEqual(hubDevice.Product, 99)
         
+    
+
+    def test_unknown_manufacturer_with_fut_device_return_developper_manufacturer_and_fit_product(self):
+        unknown_manufacturer_std_device =  {
+            "@context": "/v2/contexts/UserDevice",
+            "@id": "/v2/user_devices/eu23218ff9b8010d294e",
+            "@type": "UserDevice",
+            "id": "eu23218ff9b8010d294e",
+            "serial": "30:67:71:B8:DB:02",
+            "fitManufacturer": 294,
+            "fitDevice": 5,
+            "model": "/v2/device_models/99",
+            "firmware": "/v2/firmware/9928",
+            "user": "/v2/users/eu200a4d76c4eab29015"
+        }
+
+        # When 
+        hubDevice = DecathlonService.convertStdDeviceToHubDevice(unknown_manufacturer_std_device)
+
+        # Then
+
+        self.assertIsNotNone(hubDevice)
+        self.assertIsInstance(hubDevice, Device)
+        self.assertEqual(hubDevice.Manufacturer, "developper")
+        self.assertEqual(hubDevice.Product, 5)
